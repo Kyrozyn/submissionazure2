8a2d037aba11c1b86e0bd27978023a9414ee7434
Andreas <arizkirizaldi@email.unikom.ac.id>
2019-05-02 20:54:11 +0700

    fix https

--- analyze.php
+++ analyze.php
@@ -5,7 +5,7 @@ if(!isset($_POST['link'])){
 
 $url = $_POST['link'];
 ?>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 <script type="text/javascript">
     $(document).ready(function () {
         // **********************************************
